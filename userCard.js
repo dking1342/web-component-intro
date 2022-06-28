01e45a1a532643e5a6260a704b89ddb365d1a38b
David <leanmachine42@yahoo.com>
2022-06-28 18:52:57 -0500

    made a shadow dom component

--- userCard.js
+++ userCard.js
@@ -1,8 +1,51 @@
 
+const template = document.createElement("template");
+template.innerHTML = `
+  <style>
+    .user-card {
+      font-family:"Arial",sans-serif;
+      background-color: #f4f4f4;
+      width: 500px;
+      display: grid;
+      grid-template-columns: 1fr 2fr;
+      gap:10px;
+      margin-bottom: 15px;
+      border-bottom: darkorchid 5px solid;
+    }
+    
+    .user-card img {
+      width: 100%;
+    }
+    
+    .user-card button {
+      cursor: pointer;
+      background-color: darkorchid;
+      color:#fff;
+      border:0;
+      border-radius: 5px;
+      padding: 5px 10px;
+    }    
+  </style>
+  <div class="user-card">
+    <img />
+    <div>
+      <h3></h3>
+      <div class="info">
+        <p>Email</p>
+        <p>Phone</p>
+      </div>
+      <button id="toggle-info">Hide Info</button>
+    </div>
+  </div>
+`;
+
 class UserCard extends HTMLElement {
   constructor(){
     super();
-    this.innerHTML = `<h3>${this.getAttribute("name")}</h3>`;
+    this.attachShadow({mode:"open"});
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.querySelector("h3").innerText = this.getAttribute("name");
+    this.shadowRoot.querySelector("img").src = this.getAttribute("avatar");
   }
 
 
